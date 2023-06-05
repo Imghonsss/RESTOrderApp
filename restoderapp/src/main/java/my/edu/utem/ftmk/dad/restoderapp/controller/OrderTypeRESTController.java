69a408a9dda6bbe900141a3797a3e41dd06e3642
Imghonsss <117746756+Imghonsss@users.noreply.github.com>
2023-06-05 19:52:38 +0800

    lab 11

--- restoderapp/src/main/java/my/edu/utem/ftmk/dad/restoderapp/controller/OrderTypeRESTController.java
+++ restoderapp/src/main/java/my/edu/utem/ftmk/dad/restoderapp/controller/OrderTypeRESTController.java
@@ -1,6 +1,8 @@
 package my.edu.utem.ftmk.dad.restoderapp.controller;
 
 import java.util.List;
+import java.util.ArrayList;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -64,4 +66,114 @@ public class OrderTypeRESTController {
 	  orderTypeRepository.deleteById(orderTypeId); 
 	  return new ResponseEntity<>(HttpStatus.OK); 
 	 } 
+	 
+	 /*
+		 * Lab: 11 Task 2 - Find Order Type by Name
+		 * Method find order type data according to the order type's name.
+		 * 
+		 * @param orderType
+		 * @return A list of order types record.
+		 */
+		@RequestMapping("/find/name")
+		public List<OrderType> findOrderType (@RequestBody OrderType orderType)
+		{
+			List<OrderType> orderTypes = orderTypeRepository.findByNameContaining(orderType.getName());
+			return orderTypes;
+		}
+
+		/*
+		 * Lab: 11 Task 3 - Find Order Type by Code
+		 * Method find order type data according to the order type's partial code.
+		 * 
+		 * @param partCode
+		 * @return A list of order types record.
+		 */
+		@RequestMapping("/find/code/{partCode}")
+		public List<OrderType> findOrderType(@PathVariable String partCode)
+		{
+			List<OrderType> orderTypes = orderTypeRepository.findByCodeStartingWith(partCode);
+			return orderTypes;
+		}
+		
+		/*
+		 * Lab: 11 Task 4 - Count Order Type Data
+		 * Method count number of order type data.
+		 * 
+		 * @return A long data type of number
+		 */
+		@RequestMapping("/count")
+		public long countOrderType()
+		{
+			long number = orderTypeRepository.count();
+			return number;
+		}
+		
+		/*
+		 * Lab: 11 Task 5 - Retrieve a sorted Order Type Data
+		 * Method retrieves a list of order type data sorted in ascending order.
+		 * 
+		 * @param orderType
+		 * @return A list of order type data
+		 */
+		@GetMapping("/asc")
+		public List<OrderType> findSortedOrderTypeName()
+		{
+			return orderTypeRepository.findByOrderByNameAsc();
+		}
+		
+		/*
+		 * Lab: 11 Task 8.2
+		 * This method demonstrate the invocation of custom query and return the
+		 * result in Object form.
+		 * 
+		 * @return A list of objects where value of each field in separated arrays
+		 */
+		@GetMapping("/find/pickup/raw")
+		public List<Object[]> getRawPickUpOrderCode(){
+			
+			// Execute query method
+			List<Object[]> objOrderTypes = orderTypeRepository.selectCustomByCode();
+			
+			// For debugging purpose
+			for(Object[] objOrderType: objOrderTypes) {
+				
+				for(Object currentObject: objOrderType) {
+					
+					System.out.println(currentObject.toString());
+				}
+			}
+			
+			return objOrderTypes;
+		}
+			
+		
+		/**
+		 * Lab: 11 Task 8.4
+		 * This method demonstrate the invocation of custom query
+		 * 
+		 * @return A list of objects where result of query execution wrap in
+		 * OrderType
+		 */
+		@GetMapping("/find/pickup/wrap")
+		public List<OrderType> getWrapPickUpOrderCode() {
+			
+			// Execute query method
+			List<Object[]> objOrderTypes = orderTypeRepository.selectCustomByCode();
+			
+			// Wrap result in a list of order type
+			List<OrderType> orderTypes = new ArrayList<OrderType>();
+			for (Object[] objOrderType:objOrderTypes) {
+				
+				// Wrap in order type
+				OrderType orderType = new OrderType();
+				orderType.setCode(objOrderType[0].toString());
+				orderType.setName(objOrderType[1].toString());
+				
+				// Add into list
+				orderTypes.add(orderType);
+			}
+			
+			return orderTypes;
+		}
+
 }
--- restoderapp/src/main/java/my/edu/utem/ftmk/dad/restoderapp/repository/OrderTypeRepository.java
+++ restoderapp/src/main/java/my/edu/utem/ftmk/dad/restoderapp/repository/OrderTypeRepository.java
@@ -1,12 +1,47 @@
 package my.edu.utem.ftmk.dad.restoderapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.data.jpa.repository.Query;
 import my.edu.utem.ftmk.dad.restoderapp.model.OrderType;
 
 
 @Repository
 public interface OrderTypeRepository extends JpaRepository<OrderType, Long> {
+	
+	/*
+	 * LAB11: Task 2.1
+	 * New abstract method named findByNameContaining()
+	 * 
+	 * @Param partCode
+	 * @Return A list of order type data with successfully
+	 */
+	public abstract List<OrderType> findByNameContaining (String partName);
+	
+	/*
+	 * LAB11: Task 3.1
+	 * New abstract method named findByCodeStartingWith()
+	 * 
+	 * @Param partCode
+	 * @Return A list of order type data
+	 */
+	public abstract List<OrderType> findByCodeStartingWith(String partCode);
+	
+	/*
+	 * LAB 11: Task 5.1
+	 * New abstract method named findByOrderByNameAsc()
+	 * @Return A list of order type data
+	 */
+	public abstract List<OrderType> findByOrderByNameAsc();
+	
+	
+	// Custom query
+	@Query(value = "select * from OrderType where code like '%PU%'",
+			nativeQuery = true)
+	public List<Object[]> selectCustomByCode();
+	//public List<OrderType> findByCode();
 
+	
 }
